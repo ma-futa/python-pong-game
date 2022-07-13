acbe3d22df3017ed7a6f8f2bd4784de20ebe814f
mohammed <mohammedfuta@gmail.com>
2022-07-13 14:27:42 +0000

    version with Angela's bounce mechanics

--- ball.py
+++ ball.py
@@ -8,27 +8,14 @@ class Ball(Turtle):
         self.color('purple')
         # self.speed('slow')
         self.penup()
-        self.y_direction = True
-        self.x_direction = True
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        if self.ycor() > 250:
-            self.y_direction = False
-        if self.ycor() < -250:
-            self.y_direction = True
+        if self.ycor() > 250 or self.ycor() < -250:
+            self.y_move *= -1
 
-        if self.xcor() > 350:
-            self.x_direction = False
-        if self.xcor() < -350:
-            self.x_direction = True
+        if self.xcor() > 350 or self.xcor() < -350:
+            self.x_move *= -1
 
-        if self.y_direction:
-            new_y = self.ycor() + 10
-        else:
-            new_y = self.ycor() - 10
-
-        if self.x_direction:
-            new_x = self.xcor() + 10
-        else:
-            new_x = self.xcor() - 10
-        self.goto(new_x,new_y)
+        self.goto(self.xcor() + self.x_move,self.ycor()+self.y_move)
